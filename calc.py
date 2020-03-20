71a3b7975fdfe9bcdc44eaa5a3ef58f0f19ab4a9
frostah <gsfrost23@icloud.com>
2020-03-20 16:58:10 +0300

    Initial commit.

---
+++ calc.py
@@ -0,0 +1,12 @@
+print("Welcome to calc v0.1")
+print("Please, enter two int numbers in correct order")
+a = int(input())
+b = int(input())
+print("Please, choose the operation: enter the correct number of operation")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+operation = int(input())
+if operation == 1:
+    print(a, " + ", b, " = ", a+b)
